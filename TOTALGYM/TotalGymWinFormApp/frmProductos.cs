c9fc92837992233bc8f174966e8ffee69a9e23c6
cheecoo5 <cheecoo@live.com>
2016-07-22 12:03:53 -0500

    se crearon las carpetas

---
+++ TOTALGYM/TotalGymWinFormApp/frmProductos.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TotalGymWinFormApp
+{
+    public partial class frmProductos : Form
+    {
+        public frmProductos()
+        {
+            InitializeComponent();
+        }
+
+        private void lblnombre_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbldescripcion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmProductos_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
