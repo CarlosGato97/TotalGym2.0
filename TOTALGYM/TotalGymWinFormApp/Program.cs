b991e57c2d2debf81a180af62c2dedce67ec0d64
Deysi19 <deysimaria_19@hotmail.com>
2016-08-03 13:10:34 -0500

    se edito el login

--- TOTALGYM/TotalGymWinFormApp/Program.cs
+++ TOTALGYM/TotalGymWinFormApp/Program.cs
@@ -16,7 +16,7 @@ namespace TotalGymWinFormApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMenuPrincipal());
+            Application.Run(new frmLogin());
         }
     }
 }
